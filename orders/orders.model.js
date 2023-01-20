c2e65847f81daefbd00c1e9bd6ba73fc155bb908
Kafui Alordo <kalordo7@gmail.com>
2023-01-20 14:12:21 +0000

    modularize GraphQL Schemas using GraphQL Tools

---
+++ orders/orders.model.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    date: "2005-05-05",
+    subtotal: 90.22,
+    items: [
+      {
+        product: {
+          id: "redshoe",
+          description: "Old Red Shoe",
+          price: 45.11,
+        },
+        quantity: 2,
+      },
+      {
+        product: {
+          id: "blueshoe",
+          description: "Old Blue Shoe",
+          price: 445.11,
+        },
+        quantity: 4,
+      },
+    ],
+  },
+];
