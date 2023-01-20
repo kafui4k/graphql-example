3570c0e6de0584d8a6dda3e331eabc3e15060520
Kafui Alordo <kalordo7@gmail.com>
2023-01-20 13:53:23 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const { buildSchema } = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
+
+const schema = buildSchema(`
+    type Query {
+        products: [Product]
+        orders: [Order]
+    }
+
+    type Product {
+        id: ID!
+        description: String!
+        reviews: [Review]
+        price: Float!
+    }
+
+    type Review {
+        rating: Int!
+        comment: String
+    }
+
+    type Order {
+        date: String!
+        subtotal: Float!
+        items: [OrderItem]
+    }
+
+    type OrderItem {
+        product: Product!
+        quantity: Int!
+    }
+`);
+
+const root = {
+  products: [
+    {
+      id: "redshoe",
+      description: "Red Shoe",
+      price: 42.12,
+    },
+    {
+      id: "bluejean",
+      description: "Blue Jeans",
+      price: 55.55,
+    },
+  ],
+  orders: [
+    {
+      date: "2005-05-05",
+      subtotal: 90.22,
+      items: [
+        {
+          product: {
+            id: "redshoe",
+            description: "Old Red Shoe",
+            price: 45.11,
+          },
+          quantity: 2,
+        },
+        {
+          product: {
+            id: "blueshoe",
+            description: "Old Blue Shoe",
+            price: 445.11,
+          },
+          quantity: 4,
+        },
+      ],
+    },
+  ],
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(3000, () => {
+  console.log("GraphQL Server running....");
+});
