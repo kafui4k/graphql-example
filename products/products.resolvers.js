cef1429c18741b6781e5741d53637d378ad1c3eb
Kafui Alordo <kalordo7@gmail.com>
2023-01-21 00:50:35 +0000

    query products by id

--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -8,5 +8,8 @@ module.exports = {
     productsByPrice: (_, args) => {
       return productsModel.getProductsByPrice(args.min, args.max);
     },
+    productById: (_, args) => {
+      return productsModel.getProductById(args.id);
+    },
   },
 };
